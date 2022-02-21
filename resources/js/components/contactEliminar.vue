ebafe196cb75ba73b5a3bb56b9b6f776c172b052
UrielMorales <urielmorales@gmail.com>
2022-02-21 15:37:36 -0600

    commit inicial

---
+++ resources/js/components/contactEliminar.vue
@@ -0,0 +1,19 @@
+<template>
+	<button @click="eliminar(contact.id)" class="btn btn-danger">Eliminar</button>
+</template>
+<script>
+
+	// export default {
+	// 	name: contactEliminar,
+	// 	props:{
+	// 		ContactEliminar: null
+	// 	},
+	// 	methods: {
+	// 		async eliminar(id) {
+	// 			const res = await axios.delete('/contacts/' +id);
+	// 			this.list();
+	// 		},
+    //     }
+
+	// }
+</script>
